4890dd2597394f9bc7be1a50c6f6bae2f84fa883
Edwin <edwin@encome.in>
2022-09-07 17:19:20 +0530

    update-07092022

--- bolt/script/script.js
+++ bolt/script/script.js
@@ -10,4 +10,37 @@ jQuery(document).ready(function(){
 	    }
 	});
 
+
+	$('.client-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		responsive: {
+			0: {
+				items: 4,
+				margin: 10,
+			},
+			576: {
+				items: 4,
+				margin: 30,
+			},
+			768: {
+				items: 5,
+				margin: 30,
+			},
+			992: {
+				items: 6,
+				margin: 30,
+			},
+			1200: {
+				items: 6,
+				margin: 40,
+			},
+			1400: {
+				items: 8,
+				margin: 60,
+			}
+		}
+	});
+
 }); //Script End
