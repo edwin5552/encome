8ff09ae6edd0e880b92e7c0f80a877bb803618ed
Edwin <edwin@encome.in>
2022-12-01 11:57:50 +0530

    Update script.js

--- tables/js/script.js
+++ tables/js/script.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function(){
 
-	
+	$("#tableWebRanking").tablesorter({
+			
+		widthFixed : true,
+		widgets: [ 'zebra', 'cssStickyHeaders', 'filter' ],
+
+		widgetOptions: {
+			// cssStickyHeaders_addCaption    : true,
+			// cssStickyHeaders_attachTo      : null,
+			// cssStickyHeaders_filteredToTop : true,
+			// cssStickyHeaders_offset: 0,
+			
+			filter_reset : '.reset',
+			filter_searchFiltered : false,
+		}
+
+
+	});
 
 });
